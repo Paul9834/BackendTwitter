72f3b9f11aae667b3b2272bedb94a26916956040
Kevin Paul Montealegre Melo <kmontealegremelo@gmail.com>
2020-09-21 19:18:31 -0500

    post controllers and route

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -2,11 +2,6 @@ const dbManager = require ('../database.config/database.manager');
 const { Op } = require("sequelize");
 
 
-/**
- * Creation of a post
- * @param {*} postObject JSON Object with User information
- */
-
 async function createPost(req, res) {
 
     // CHECK IF THE REQUEST BODY IS EMPTY
@@ -17,14 +12,12 @@ async function createPost(req, res) {
         return;
     }
 
-
     // CREATING THE OBJECT TO PERSIST
     const newPostObject = ({
         message: req.body.message,
         published_date: req.body.published_date,
         idUser : req.body.idUser
     })
-
 
     // EXECUTING THE CREATE QUERY - INSERT THE OBJECT INTO DATABASE
     dbManager.Post.create(newPostObject).then (
@@ -44,62 +37,9 @@ async function createPost(req, res) {
     );
 }
 
-/**
- * GEt all users
- */
-async function findAllPost (req, res){
-    try {
-        //Execute query
-        const users = await dbManager.User.findAll ();
 
-        //Send response
-        res.json({
-            data: users
-        });
+async function updatePost(req, res) {
 
-    } catch (e) {
-        // Print error on console
-        console.log(e);
-        // Send error message as a response
-        res.status(500).send({
-            message: "Some error occurred"
-        });
-    }
-}
-
-/**
- * Get user by id
- */
-async function findOnePost(req, res){
-    try {
-        const { idUser } = req.params;
-
-        //Execute query
-        const user = await dbManager.User.findOne({
-            where: {
-                idUser: idUser
-            }
-        });
-        //Send response
-        res.json(user);
-
-    } catch (e) {
-        // Print error on console
-        console.log(e);
-        // Send error message as a response
-        res.status(500).send({
-            message: "Some error occurred"
-        });
-    }
-}
-/**
- * Update user
- */
-async function updatePost (req, res) {
-    /**
-     * TASK:
-     * IMPLEMENT THE FUNCTION________-
-     */
     // CHECK IF THE REQUEST BODY IS EMPTY
     if (!req.body) {
         res.status(400).send({
@@ -108,70 +48,63 @@ async function updatePost (req, res) {
         return;
     }
 
-}
+    if (!req.params) {
+        res.status(400).send({
+            message: "Request params is empty!!!!"
+        });
+        return;
+    }
 
-function deleteAllPosts(req, res) {
-    /**
-     * TASK:
-     * IMPLEMENT THE FUNCTION______________________-
-     */
+    const postUpdate = {
+        message: req.body.message
+    }
 
-    //[Op.gt] : 0}
+    const id_post = req.params.idPost
 
-    dbManager.User.destroy({where: {idUser: {[Op.gt]: 0}}}).then(
-        res.send("Se elimino todos los usuarios correctamente")
-    ).catch(
-        e => {
-            // Print error on console
-            console.log(e);
-            // Send error message as a response
-            res.status(500).send({
-                message: "No se pudo eliminar todos los usuarios."
-            });
-        }
-    );
-
-}
-
-/**
- *
- * @param {*} req
- * @param {*} res
- */
-function findAllPostByCreatedDate(req, res) {
-
-    /**
-     * TASK:
-     * IMPLEMENT THE FUNCTION______________________-
-     */
-
-    const fecha = req.params.creation_date
-
-    dbManager.User.findAll( { where: {
-
-            creation_date: { [Op.between]: [fecha, fecha]}} }).then(
-
+    dbManager.Post.update(postUpdate, { where: {idPost: id_post}}).then (
         data => {
             res.send(data);
         }
+    ).catch (
+        e => {
+            // Print error on console
+            console.log(e);
+            // Send error message as a response
+            res.status(500).send({
+                message: "No se pudo actualizar el post"
+            });
+        }
+    );
+
+}
+
+async function deletePost(req, res) {
+
+    if (!req.params) {
+        res.status(400).send({
+            message: "Request params is empty!!!!"
+        });
+        return;
+    }
+
+    const idPost_params = req.params.idPost
+
+    dbManager.Post.destroy({where: {idPost_params: idPost_params}}).then(
+        res.send("El post se elimino correctamente")
     ).catch(
         e => {
             // Print error on console
             console.log(e);
             // Send error message as a response
             res.status(500).send({
-                message: "No se pudo eliminar todos los usuarios."
+                message: "No se pudo eliminar el post."
             });
         }
     );
+
 }
 
 
-
 exports.createPost = createPost;
-exports.findAllPost = findAllPost;
-exports.findOnePost = findOnePost;
 exports.updatePost =  updatePost;
-
-exports.deleteAllPosts = deleteAllPosts;
-exports.findAllPostByCreatedDate = findAllPostByCreatedDate;
+exports.deletePost = deletePost;
--- routes/posts.route.js
+++ routes/posts.route.js
@@ -4,16 +4,29 @@ const postController = require ('../controllers/post.controller');
 
 
 /**
- * TASK:
- * CREATE THE ROUTES _________________________________________________________ 
- */
-
-
-/**
+ *
  * POST Route to create user
+ *
  */
 
 router.post ('/',postController.createPost);
+
+/**
+ *
+ * PUT Route to update an user by id
+ *
+ */
+router.put ('/:idPost',postController.updatePost);
+
+
+/**
+ *
+ * DELETE Route to delete an post by id
+ *
+ */
+router.delete('/:idPost', postController.deletePost);
+
+
 
 module.exports = router;
   
